f8f6f58cb2ce57ce8de10241ce5e206542b3eafb
Nguyen Hieu <100512055+Mhieu100@users.noreply.github.com>
2024-08-15 15:03:14 +0700

    thống kê lỏ mới version 1
    :100: :alarm_clock:

--- src/main/java/group7/springmvc/controller/admin/DashboardController.java
+++ src/main/java/group7/springmvc/controller/admin/DashboardController.java
@@ -1,24 +1,62 @@
 package group7.springmvc.controller.admin;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import group7.springmvc.model.User;
+import group7.springmvc.service.ScheduleService;
 import group7.springmvc.service.UserService;
+import group7.springmvc.service.VaccineService;
 
 @Controller
 @RequestMapping("/admin")
 public class DashboardController {
 
-	@GetMapping("/")
-	public String dashboard() {
-		return "admin/dashboard/index";
-	}
-	
 	@Autowired
 	UserService userService;
-	
+
+	@Autowired
+	VaccineService vaccineService;
+
+	@Autowired
+	ScheduleService scheduleService;
+
+	@GetMapping("/")
+	public String dashboard(Model model) {
+		List<User> listUser = userService.findAll();
+		model.addAttribute("sizeUser", listUser.size());
+		model.addAttribute("sizeVaccine", vaccineService.findAll().size());
+		model.addAttribute("sizeSchedule", scheduleService.getAllSchedules().size());
+		int numberADMIN = 0;
+		int numberDOCTOR = 0;
+		int numberRECEPTIONIST = 0;
+		int numberPATIENT = 0;
+		for (User user : listUser) {
+			String roleValue = user.getRole().getValue().toString();
+			if (roleValue.equals("ADMIN")) {
+				++numberADMIN;
+			} else if (roleValue.equals("DOCTOR")) {
+				++numberDOCTOR;
+			} else if (roleValue.equals("RECEPTIONIST")) {
+				++numberRECEPTIONIST;
+			} else if (roleValue.equals("PATIENT")) {
+				++numberPATIENT;
+			}
+
+		}
+		model.addAttribute("numberADMIN", numberADMIN);
+		model.addAttribute("numberDOCTOR", numberDOCTOR);
+		model.addAttribute("numberRECEPTIONIST", numberRECEPTIONIST);
+		model.addAttribute("numberPATIENT", numberPATIENT);
+
+		return "admin/dashboard/index";
+	}
+
 	@GetMapping("/profile")
 	public String profile(Model model) {
 		return "admin/auth/profile";
